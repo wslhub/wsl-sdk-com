19c92e19dc80bdef83fd70a57a25bb5fecbefc69
Jung Hyun Nam <rkttu@rkttu.com>
2021-06-19 15:33:04 +0900

    Add unit test

---
+++ WslSdk.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WslSdk.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WslSdk.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5ea3f3a-3379-47f5-aa5a-03f0ba61a7b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WslSdk.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WslSdk.Test
+{
+    [TestClass]
+    public class DistroEnumTest
+    {
+        [TestMethod]
+        public void Test_GetDefaultDistro()
+        {
+            var wslServiceType = Type.GetTypeFromProgID("WslSdk.WslService");
+            dynamic wslService = Activator.CreateInstance(wslServiceType);
+            dynamic distroInfo = wslService.GetDefaultDistro();
+            Assert.AreNotEqual(distroInfo.DistroId(), null);
+        }
+    }
+}
