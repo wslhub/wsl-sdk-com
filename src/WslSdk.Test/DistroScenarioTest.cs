a0ec30bd518519aecad4871664216a97dc8d9e94
Jung Hyun Nam <rkttu@rkttu.com>
2021-06-26 16:53:28 +0900

    Split manipulation test and file system test - Add scenario test

---
+++ src/WslSdk.Test/DistroScenarioTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WslSdk.Test
+{
+    [TestClass]
+    public class DistroScenarioTest
+    {
+        private dynamic ActivateWslService()
+        {
+            var wslServiceType = Type.GetTypeFromProgID("WslSdk.WslService");
+            dynamic wslService = Activator.CreateInstance(wslServiceType);
+            return wslService;
+        }
+
+        [TestMethod]
+        public void Test_CurlSimple()
+        {
+            dynamic wslService = ActivateWslService();
+            var defaultDistroName = wslService.GetDefaultDistroName();
+            var res = wslService.RunWslCommand(defaultDistroName, "curl https://api.ipify.org");
+
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.Length > 0);
+        }
+
+        [TestMethod]
+        public void Test_CurlLargeContent()
+        {
+            dynamic wslService = ActivateWslService();
+            var defaultDistroName = wslService.GetDefaultDistroName();
+            var res = wslService.RunWslCommand(defaultDistroName, "curl https://www.google.com");
+
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.Length > 0);
+        }
+    }
+}
