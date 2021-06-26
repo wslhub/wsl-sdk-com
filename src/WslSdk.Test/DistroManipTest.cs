0036a1d7892dc632bbc11d8d213827d380682ced
Jung Hyun Nam <rkttu@rkttu.com>
2021-06-26 14:25:11 +0900

    Fix test cases

--- src/WslSdk.Test/DistroManipTest.cs
+++ src/WslSdk.Test/DistroManipTest.cs
@@ -75,12 +75,49 @@ namespace WslSdk.Test
             var tempDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WslSdkTest", randomName);
 
             wslService.RegisterDistro(randomName, busyboxRootfsFile, tempDirectory);
-            var res = wslService.TranslateToWindowsPath(randomName, "/usr/bin");
-            wslService.UnregisterDistro(randomName);
+            var res = wslService.TranslateToWindowsPath(randomName, "/bin");
 
             Assert.IsNotNull(res);
             Assert.IsTrue(res.Length > 0);
             Assert.IsTrue(Directory.Exists(res));
+
+            wslService.UnregisterDistro(randomName);
+        }
+
+        [TestMethod]
+        public void Test_WindowsToLinuxPath()
+        {
+            dynamic wslService = ActivateWslService();
+            var randomName = wslService.GenerateRandomName(true);
+            var busyboxRootfsFile = Path.GetFullPath("busybox.tgz");
+            var tempDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WslSdkTest", randomName);
+
+            wslService.RegisterDistro(randomName, busyboxRootfsFile, tempDirectory);
+            var res = wslService.TranslateToLinuxPath(randomName, @"C:\\Windows");
+
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.Length > 0);
+            Assert.AreEqual("/mnt/c/Windows", res);
+
+            wslService.UnregisterDistro(randomName);
+        }
+
+        [TestMethod]
+        public void Test_WindowsToLinuxPath_Recursive()
+        {
+            dynamic wslService = ActivateWslService();
+            var randomName = wslService.GenerateRandomName(true);
+            var busyboxRootfsFile = Path.GetFullPath("busybox.tgz");
+            var tempDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WslSdkTest", randomName);
+
+            wslService.RegisterDistro(randomName, busyboxRootfsFile, tempDirectory);
+            var res = wslService.TranslateToLinuxPath(randomName, $@"\\\\wsl$\\{randomName}\\bin");
+
+            Assert.IsNotNull(res);
+            Assert.IsTrue(res.Length > 0);
+            Assert.AreEqual("/bin", res);
+
+            wslService.UnregisterDistro(randomName);
         }
     }
 }
