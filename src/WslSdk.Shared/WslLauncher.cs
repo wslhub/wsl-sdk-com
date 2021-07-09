a90266a1d29989d71cf0481fea8eb957f60e21dd
Jung Hyun Nam <rkttu@rkttu.com>
2021-07-09 09:46:38 +0900

    Add encoding analysis

--- src/WslSdk.Shared/WslLauncher.cs
+++ src/WslSdk.Shared/WslLauncher.cs
@@ -11,6 +11,35 @@ namespace WslSdk.Shared
     {
         public const int DefaultBufferSize = 65536;
 
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        public static Encoding GetEncoding(byte[] dataFragment)
+        {
+            // Code from: https://stackoverflow.com/questions/3825390/effective-way-to-find-any-files-encoding
+            var defaultEncoding = new UTF8Encoding(false);
+
+            if (dataFragment.Length < 4)
+                return defaultEncoding;
+
+            // Read the BOM
+            var bom = new byte[4];
+            Array.Copy(dataFragment, bom, 4);
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) return Encoding.UTF32; //UTF-32LE
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return new UTF32Encoding(true, true);  //UTF-32BE
+
+            return defaultEncoding;
+        }
+
         public static int? RunWslCommandAsStream(
             WslApiLoader apiLoader,
             string distroName,
@@ -59,12 +88,10 @@ namespace WslSdk.Shared
             Action<string> stderr = null,
             int bufferSize = DefaultBufferSize)
         {
-            targetEncoding = targetEncoding ?? new UTF8Encoding(false);
-
             return RunWslCommandAsStream(apiLoader, distroName, command, useCurrentWorkingDirectory, out stdinWrittenByte,
                 stdin: stdin,
-                stdout: data => stdout?.Invoke(targetEncoding.GetString(data, 0, data.Length)),
-                stderr: data => stderr?.Invoke(targetEncoding.GetString(data, 0, data.Length)),
+                stdout: data => stdout?.Invoke((targetEncoding ?? GetEncoding(data)).GetString(data, 0, data.Length)),
+                stderr: data => stderr?.Invoke((targetEncoding ?? GetEncoding(data)).GetString(data, 0, data.Length)),
                 bufferSize);
         }
 
